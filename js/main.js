7ea4d9c95c970ce8a841842898affda84ee2ac92
thaibaole86 <thaibaole86@outlook.com>
2021-07-19 22:07:27 +0700

    1st

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', addmenu)
+function addmenu() {
+    let menu = [
+        { id:"m-home" , title: "Home", url: "#home", f: "onclick='load_page_home()'" },
+        { id: "m-globalsim", title: "Global SIM", url: "#globalsim", f: "onclick='load_page_globalsim()'"  },
+        { id: "m-esim", title: "E-SIM", url: "#esim", f: "onclick='load_page_esim()'"  },
+        { id: "m-contact", title: "Contact", url: "#contact", f: "onclick='load_page_contact()'" }
+    ]       
+
+    for (i = 0; i <= menu.length; i++ ) {
+        document.getElementById('menu-items').innerHTML += "<li><a id=" + menu[i].id + " href=" + menu[i].url + ">" + menu[i].title + "</a></li>"
+    }
+}
+
+// Mỗi lần load web thì Check xem # trên URL nó là gì thì sẽ insert phần HTML có id giống tên nó mà đang hide trên trang.
+window.addEventListener('load', displayHash)
+function displayHash() {
+    let theHash = window.location.hash;
+    let pagecontentload = document.getElementById("main-body-display");
+    pagecontentload.innerHTML = document.getElementById(theHash).innerHTML;
+  }
+
+  // Mỗi lần # trên URL thay đổi thì chạy lại lệnh displayHash
+window.addEventListener("hashchange", function() {
+    console.log("hashchange event");
+    displayHash();
+  });
+
+
+function newmenuitem() {
+    document.getElementById('menu-items').innerHTML += "<li><a id=" + menu[i].id + " href=" + menu[i].url + ">" + menu[i].title + "</a></li>"
+}
