30f3db4e1a65c1dc8c67b37d27a6a3c00ee5fd4c
Seiko Bao <thaibaole86@outlook.com>
2021-07-23 22:46:50 +0700

    new

---
+++ admin/_products/jewelry.md
@@ -0,0 +1,6 @@
+---
+thumbnail: /images/uploads/october-1-1-.jpg
+title: Jewelry
+price: 5
+description: Here is cool
+---
--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-window.addEventListener('load', addmenu)
+window.addEventListener('load', addmenu);
+window.addEventListener('load', addsanpham);
+window.addEventListener('load', displayHash)
+
 function addmenu() {
     let menu = [
-        { id:"m-home" , title: "Home", url: "#home", f: "onclick='load_page_home()'" },
-        { id: "m-globalsim", title: "Global SIM", url: "#globalsim", f: "onclick='load_page_globalsim()'"  },
-        { id: "m-esim", title: "E-SIM", url: "#esim", f: "onclick='load_page_esim()'"  },
-        { id: "m-contact", title: "Contact", url: "#contact", f: "onclick='load_page_contact()'" }
+        { title: "Home", url: "#home", f: "onclick='load_page_home()'" },
+        { title: "Global SIM", url: "#globalsim", f: "onclick='load_page_globalsim()'"  },
+        { title: "E-SIM", url: "#products", f: "onclick='load_page_esim()'"  },
+        { title: "Contact", url: "#contact", f: "onclick='load_page_contact()'" }
     ]       
 
-    for (i = 0; i <= menu.length; i++ ) {
-        document.getElementById('menu-items').innerHTML += "<li><a id=" + menu[i].id + " href=" + menu[i].url + ">" + menu[i].title + "</a></li>"
+    for (i = 0; i < menu.length; i++ ) {
+        document.getElementById('menu-items').innerHTML += "<li><a href=" + menu[i].url + ">" + menu[i].title + "</a></li>"
     }
 }
 
 // Mỗi lần load web thì Check xem # trên URL nó là gì thì sẽ insert phần HTML có id giống tên nó mà đang hide trên trang.
-window.addEventListener('load', displayHash)
+
 function displayHash() {
     let theHash = window.location.hash;
     if (theHash.length == 0) { theHash = "#home"; }
@@ -30,4 +33,78 @@ window.addEventListener("hashchange", function() {
 
 function newmenuitem() {
     document.getElementById('menu-items').innerHTML += "<li><a id=" + menu[i].id + " href=" + menu[i].url + ">" + menu[i].title + "</a></li>"
+}
+
+// Sản phẩm
+
+function addsanpham() {
+    let sanpham = [
+    {
+        id: 1, 
+        title: "Asia 17 countries",
+        image: "/img/asia.jpg",
+        slug:"asia",
+        package: [{data:"1GB",price: 10}, {data:"5GB", price: 20}, {data:"10GB", price: 30}],
+        category: "asia",
+        description: "Asia Economy Packages" 
+    },
+
+    { 
+        id: 2, 
+        title: "Asia Premium 22 countries",
+        image: "/img/asia.jpg",
+        slug:"asia-premium",
+        package: [{data:"6GB",price: 10}, {data:"10GB", price: 20}, {data:"30GB", price: 30}],
+        category: "asia",
+        description: "Asia Economy Packages" 
+    },
+    {
+        id: 3, 
+        title: "Middle East Region",
+        image: "/img/asia.jpg",
+        slug:"middle-east",
+        package: [{data:"6GB",price: 10}, {data:"10GB", price: 20}, {data:"30GB", price: 30}],
+        category: "middle-east",
+        description: "Middle East Packages" 
+    },
+    {
+        id: 4, 
+        title: "Africa Region",
+        image: "/img/asia.jpg",
+        slug:"africa",
+        package: [{data:"6GB",price: 10}, {data:"10GB", price: 20}, {data:"30GB", price: 30}],
+        category: "africa",
+        description: "Africa Packages" 
+    },
+    {
+        id: 5, 
+        title: "Europe 46 countries",
+        image: "/img/asia.jpg",
+        slug:"europe",
+        package: [{data:"6GB",price: 10}, {data:"10GB", price: 20}, {data:"30GB", price: 30}],
+        category: "africa",
+        description: "Europe Packages" 
+    },
+    {
+        id: 6, 
+        title: "World Basic 36 countries",
+        image: "/img/asia.jpg",
+        slug:"world-basic",
+        package: [{data:"6GB",price: 10}, {data:"10GB", price: 20}, {data:"30GB", price: 30}],
+        category: "world",
+        description: "World Basic Packages" 
+    },
+    ]
+
+
+    for (i = 0; i< sanpham.length; i++) {        
+        document.getElementById('product-name').innerHTML += "<div id='product-detail'>" + "<img class='product-image' src=" + sanpham[i].image + ">" + "<div class='product-title'>" + sanpham[i].title + "</div>" + "<div class='product-description'>" + sanpham[i].description + "</div>" + "<br>" + "<div id='data-package-" + sanpham[i].id +  "' ></div>" + "<button class='product-button'> Buy Now </button>" + "</div>";   
+        
+        let sppk = sanpham[i].package;
+        let dtpk = 'data-package-' + sanpham[i].id;
+
+            for (j = 0; j < sppk.length; j++) {
+            document.getElementById(dtpk).innerHTML += "<input type='radio'" + " name=" + sppk + "value=" + sppk[j] + " id=" + dtpk + "-" + sppk[j].data + " >" + "<label for=" + dtpk + "-" + sppk[j].data + ">" + sppk[j].data + " - " + sppk[j].price + "$" + "</label>" ;
+        }
+    }
 }
