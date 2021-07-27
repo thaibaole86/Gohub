747cc216236087666694d7c4abd1af6c19a5cb11
Seiko Bao <thaibaole86@outlook.com>
2021-07-27 21:16:54 +0700

    ADMIN UPDATE

--- js/admin.js
+++ js/admin.js
@@ -17,4 +17,22 @@ function addmenu(){
   })
   .then(response => response.json()) 
   .then(json => console.log(json));
-          }
+}
+
+window.addEventListener('load', fetchdatamenu);
+function fetchdatamenu(){
+  fetch('https://api.airtable.com/v0/appJuih6tuaTappGZ/menu?sort%5B0%5D%5Bfield%5D=id&sort%5B0%5D%5Bdirection%5D=asc&api_key=keySoD6lDEycOXqdZ').then(function(response) {
+      data = response.json();
+      return data;
+  })
+  .then(function(data) {
+      listmenu(data)
+      });
+}
+
+function listmenu(data) {
+  let menu = data.records;
+  for (i = 0; i < menu.length; i++ ) {
+      document.getElementById('menulist').innerHTML += "<table><tr>" + "<td>" + menu[i].fields.id + "</td><td>" + menu[i].fields.menuTitle + "</td><td>" + menu[i].fields.menuUrl + "</td></tr></table>";
+  }
+}
